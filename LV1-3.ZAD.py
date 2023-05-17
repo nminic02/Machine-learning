01910379c81abaa0b0085b1088f7e28d624a4259
nminic02 <109159620+nminic02@users.noreply.github.com>
2023-05-17 21:49:38 +0200

    Update LV1-3.ZAD.py

--- LV1-3.ZAD.py
+++ LV1-3.ZAD.py
@@ -1,14 +1,4 @@
-#####################
-#     Zadatak 3     #
-#####################
 
-"""
-    Napišite program koji od korisnika zahtijeva unos brojeva u beskonačnoj petlji sve dok korisnik ne upiše „Done“ (bez
-    navodnika). Pri tome brojeve spremajte u listu. Nakon toga potrebno je ispisati koliko brojeva je korisnik unio, njihovu
-    srednju, minimalnu i maksimalnu vrijednost. Sortirajte listu i ispišite je na ekran.
-    Dodatno: osigurajte program od pogrešnog unosa (npr. slovo umjesto brojke) na način da program zanemari taj unos i
-    ispiše odgovarajuću poruku.
-"""
 
 brojevi = []
 
